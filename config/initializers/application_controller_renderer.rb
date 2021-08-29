8bef1d1264cdb136ed026f769feec7b726a63acc
enddrop28 <mea.enddrop@gmail.com>
2021-08-29 17:50:20 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '35a813a24ddd659370b0dae7488d6a361440cc1de2f41994ee841c0784f80237e28cdedcb5f8eb2a16fa2cd6c76144db659592a251aea6ec052e5c8b37345277'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
 end
